5fa9ea8d6ea7d0b0c3f7d77db861aaa0720ffd01
sid221 <sudhanshu221b@gmail.com>
2020-11-01 06:16:22 +0530

    implemented shop page and functionality

---
+++ src/pages/ShopPage.js
@@ -0,0 +1,87 @@
+import { useDispatch, useSelector } from "react-redux";
+
+const ProductCard = ({ name, id, desc, image, price }) => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state.shop);
+  const handleAddItemInCart = (e) => {
+    e.preventDefault();
+    // console.log("add item to cart: ", name);
+    dispatch({ type: "ADD_ITEM", payload: { name, id, desc, image, price } });
+  };
+  const incrementItem = () => {
+    // console.log("Increment Product: ", name);
+    dispatch({ type: "INCREMENT_ITEM_IN_CART", payload: { id } });
+  };
+  const decrementItem = () => {
+    // console.log("Decrement Product: ", name);
+    dispatch({ type: "DECREMENT_ITEM_IN_CART", payload: { id } });
+  };
+
+  const checkItemInCart = ((itemId) => {
+    const x = !!cart ? cart.filter((item) => item.id === itemId) : [];
+    console.log(x);
+    return x;
+  })(id);
+
+  return (
+    <div className="product-card-container">
+      <div className="product-image-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="product-desc-container">
+        <h2>{name}</h2>
+        <p>
+          <span>Desc: </span>
+          {desc}
+        </p>
+      </div>
+      <div className="product-action-container">
+        <div className="product-price">Price: {price || 20}$</div>
+        {checkItemInCart.length > 0 && (
+          <div className="product-quantity">
+            Quantity: {checkItemInCart[0].quantity}
+          </div>
+        )}
+        <div className="product-action-button-container">
+          {checkItemInCart.length === 0 ? (
+            <button onClick={handleAddItemInCart}>Add to Cart</button>
+          ) : (
+            <>
+              <button onClick={incrementItem} className="cart-item-action-btn">
+                <img src="/static/icons/plus.svg" alt="+" />
+              </button>
+              <button onClick={decrementItem} className="cart-item-action-btn">
+                <img src="/static/icons/minus.svg" alt="-" />
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ShopPage = (props) => {
+  const { products } = useSelector((state) => state.shop);
+
+  const addProduct = (e) => {
+    e.preventDefault();
+    console.log("add item");
+  };
+  return (
+    <div className="shop-page-container">
+      <h1>shop</h1>
+      <div className="product-list-container">
+        {!!products ? (
+          products.map((product) => {
+            return <ProductCard key={product.id} {...product} />;
+          })
+        ) : (
+          <div>No items in Shop</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ShopPage;
--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -1,4 +1,8 @@
-import {ADD_ITEM} from "./shopActionTypes";
+import {
+  ADD_ITEM,
+  INCREMENT_ITEM_IN_CART,
+  DECREMENT_ITEM_IN_CART,
+} from "./shopActionTypes";
 
 const initialState = {
   products: [
@@ -7,37 +11,78 @@ const initialState = {
       id: 3453322,
       desc: "something about product 1",
       image: "https://i.ndtvimg.com/i/2017-02/coffee_620x318_41486141224.jpg",
+      price: 10,
     },
     {
       name: "Product 2",
       id: 34532,
       desc: "something about product 2",
       image: "https://i.ndtvimg.com/i/2017-02/coffee_620x318_41486141224.jpg",
+      price: 15,
     },
     {
       name: "Product 3",
       id: 53322,
       desc: "something about product 3",
       image: "https://i.ndtvimg.com/i/2017-02/coffee_620x318_41486141224.jpg",
+      price: 20,
     },
     {
       name: "Product 4",
       id: 34533,
       desc: "something about product 4",
       image: "https://i.ndtvimg.com/i/2017-02/coffee_620x318_41486141224.jpg",
+      price: 13,
     },
     {
       name: "Product 5",
       id: 5453322,
       desc: "something about product 5",
       image: "https://i.ndtvimg.com/i/2017-02/coffee_620x318_41486141224.jpg",
+      price: 18,
     },
   ],
   loading: false,
+  cart: [],
 };
 
 const shop = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_ITEM:
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+      };
+
+    case INCREMENT_ITEM_IN_CART:
+      // let newCart = state.cart.map((item) => {
+      //   return item.id === action.payload.id
+      //     ? { ...item, quantity: item.quantity + 1 }
+      //     : item;
+      // });
+      return {
+        ...state,
+        cart: state.cart.map((item) => {
+          return item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        }),
+      };
+
+    case DECREMENT_ITEM_IN_CART:
+      let newCart = [];
+      state.cart.forEach((item) => {
+        if (item.id === action.payload.id && item.quantity > 0) {
+          if (item.quantity - 1 === 0) {
+            return;
+          }
+          return newCart.push({ ...item, quantity: item.quantity - 1 });
+        } else {
+          return newCart.push(item);
+        }
+      });
+      return { ...state, cart: newCart };
+
     default:
       return { ...state };
   }
