bc1e73abf0f9c3a99a80dc132872526ff72f149c
sid221 <sudhanshu221b@gmail.com>
2020-11-01 10:18:34 +0530

    added product details model

--- src/reusable/ProductCard.js
+++ src/reusable/ProductCard.js
@@ -1,37 +1,67 @@
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const ProductCard = ({ name, id, desc, image, price }) => {
   const dispatch = useDispatch();
   const { cart } = useSelector((state) => state.shop);
+  const [openModel, setOpenModel] = useState(false);
   const handleAddItemInCart = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
+    e.stopPropagation();
     // console.log("add item to cart: ", name);
     dispatch({ type: "ADD_ITEM", payload: { name, id, desc, image, price } });
   };
-  const incrementItem = () => {
+  const incrementItem = (e) => {
+    e.stopPropagation();
     // console.log("Increment Product: ", name);
     dispatch({ type: "INCREMENT_ITEM_IN_CART", payload: { id } });
   };
-  const decrementItem = () => {
+  const decrementItem = (e) => {
+    e.stopPropagation();
     // console.log("Decrement Product: ", name);
     dispatch({ type: "DECREMENT_ITEM_IN_CART", payload: { id } });
   };
 
   const checkItemInCart = ((itemId) => {
     const x = !!cart ? cart.filter((item) => item.id === itemId) : [];
-    console.log(x);
     return x;
   })(id);
 
+  const toggleOpenModel = (e) => {
+    // e.preventDefault();
+    console.log("click");
+    console.log(openModel);
+    setOpenModel(!openModel);
+  };
+
   return (
-    <div className="product-card-container">
+    <div className="product-card-container" onClick={toggleOpenModel}>
+      {openModel && (
+        <div className="product-detail-model">
+          <button onClick={toggleOpenModel}>Close</button>
+          <div className="product-image-container">
+            <img src={image} alt={name} />
+          </div>
+          <div className="product-desc-container">
+            <h2>{name}</h2>
+            <p>
+              <span>
+                <strong>Desc: </strong>
+              </span>
+              {desc}
+            </p>
+          </div>
+        </div>
+      )}
       <div className="product-image-container">
         <img src={image} alt={name} />
       </div>
       <div className="product-desc-container">
         <h2>{name}</h2>
         <p>
-          <span>Desc: </span>
+          <span>
+            <strong>Desc: </strong>
+          </span>
           {desc}
         </p>
       </div>
